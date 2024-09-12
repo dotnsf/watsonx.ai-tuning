eb5837e619695ee0da33c4cd5f65db066eb58dfd
K.Kimura <dotnsf@gmail.com>
2024-09-12 19:41:25 +0900

    round result values with round_bae() function.

--- app.js
+++ app.js
@@ -42,11 +42,11 @@ async function generate_jsonl_sample1(){
         var price = pair.base + i * pair.step; 
         for( var j = 1; j <= M; j ++ ){
           var n = 10 * j;
-          var ans_price = n * price;
+          var ans_price = round_base( n * price, 3 );
 
-          var input = '1' + pair.name + 'は' + price + '円です。' + n + pair.name + 'は何円ですか？';
+          var input = '1 ' + pair.name + 'は ' + price + ' 円です。 ' + n + ' ' + pair.name + 'は何円ですか？';
           //var output = '1' + pair.name + 'が' + price + '円なので、' + ans_price + '円です。'; 
-          var output = '1' + pair.name + 'が' + price + '円なので、' + n + pair.name + 'は' + price + ' * ' +  n + ' = ' + ans_price + '円です。'; 
+          var output = '1 ' + pair.name + 'が ' + price + ' 円なので、 ' + n + ' ' + pair.name + 'は約 ' + price + ' * ' +  n + ' = ' + ans_price + ' 円です。'; 
           console.log( '{"input": "' + input + '", "output": "' + output + '"}' );
         }
       }
@@ -64,11 +64,11 @@ async function generate_jsonl_sample2(){
         var price = pair.base + i * pair.step; 
         for( var j = 1; j <= M; j ++ ){
           var n = 1 * j;
-          var ans_price = n * ( 1.0 / price );
+          var ans_price = round_base( n * ( 1.0 / price ), 3 );
 
-          var input = '1' + pair.name + 'は' + price + '円です。' + n + '円は何' + pair.name + 'ですか？';
+          var input = '1 ' + pair.name + 'は ' + price + ' 円です。 ' + n + ' 円は何' + pair.name + 'ですか？';
           //var output = '1' + pair.name + 'が' + price + '円なので、' + ans_price + pair.name + 'です。'; 
-          var output = '1' + pair.name + 'が' + price + '円なので、' + n + '円は' + n + ' * ( 1 / ' + price + ' ) = ' + ans_price + pair.name + 'です。'; 
+          var output = '1 ' + pair.name + 'が ' + price + ' 円なので、 ' + n + ' 円は約 ' + n + ' * ( 1 / ' + price + ' ) = ' + ans_price + ' ' + pair.name + 'です。'; 
           console.log( '{"input": "' + input + '", "output": "' + output + '"}' );
         }
       }
@@ -89,15 +89,23 @@ async function generate_jsonl_sample3(){
     for( var i = 0; i < arr.length; i ++ ){
       var pair1 = arr[i];
       var pair2 = arr[(i+1) % arr.length]
-      var ans_price = ( 1.0 / pair2.base ) * ( pair1.base );
+      var ans_price = round_base( ( 1.0 / pair2.base ) * ( pair1.base ), 3 );
       
-      var input = '1' + pair1.name + 'は' + pair1.base + '円で、1' + pair2.name + 'は' + pair2.base + '円です。1' + pair1.name + 'は何' + pair2.name + 'ですか？';
+      var input = '1 ' + pair1.name + 'は ' + pair1.base + ' 円で、 1 ' + pair2.name + 'は ' + pair2.base + ' 円です。 1 ' + pair1.name + 'は何' + pair2.name + 'ですか？';
       //var output = '1' + pair1.name + 'が' + pair1.base + '円で、1' + pair2.name + 'は' + pair2.base + '円なので、' + ans_price + pair2.name + 'です。'; 
-      var output = '1' + pair1.name + 'が' + pair1.base + '円で、1' + pair2.name + 'は' + pair2.base + '円なので、1' + pair1.name + 'は ( 1.0 / ' + pair2.base + ' ) * ' + pair1.base + ' = ' + ans_price + pair2.name + 'です。'; 
+      var output = '1 ' + pair1.name + 'が ' + pair1.base + ' 円で、 1 ' + pair2.name + 'は ' + pair2.base + ' 円なので、 1 ' + pair1.name + 'は約 ( 1.0 / ' + pair2.base + ' ) * ' + pair1.base + ' = ' + ans_price + ' ' + pair2.name + 'です。'; 
       console.log( '{"input": "' + input + '", "output": "' + output + '"}' );
     }
     resolve( true );
   });
+}
+
+//. 四捨五入
+function round_base( value, base ){ //. base = 1 :  小数第一位、2 : 小数第二位、3: 小数第三位、・・
+  var m = Math.pow( 10, base );
+  var r = Math.round( value * m ) / m;
+
+  return r;
 }
 
 generate_jsonl().then( function( result ){
